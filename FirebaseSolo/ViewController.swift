eb62dded18fa72cc62adb01961979b66ff4fd923
keshew <97288599+keshew@users.noreply.github.com>
2022-09-07 13:02:47 +0500

    Initial Commit

---
+++ FirebaseSolo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseSolo
+//
+//  Created by Артём Коротков on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
